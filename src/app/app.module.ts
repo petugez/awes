4691e364bb232a52c00480c7ddbaa2a1afb36d5d
Peter Ladanyi <petugez@gmail.com>
2018-09-01 21:59:54 +0200

    Awes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,30 @@ import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
 
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppRoutingModule }     from './app-routing.module';
 
 import { AppComponent }         from './app.component';
-import { DashboardComponent }   from './dashboard/dashboard.component';
-
-
+import { LoginPageComponent } from './loginpage/loginpage.component';
+//import { LoginPageComponent } from './loginpage/loginpage.component';
 
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+   
     AppRoutingModule,
-    HttpClientModule,
+    HttpClientModule
 
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
+   
   ],
   declarations: [
     AppComponent,
-    DashboardComponent/*  */
+    LoginPageComponent
   ],
   bootstrap: [ AppComponent ]
 })
---
+++ src/app/loginpage/loginpage.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'login-page',
+  templateUrl: './loginpage.component.html',
+  styleUrls: [ './loginpage.component.css' ]
+})
+export class LoginPageComponent implements OnInit {
+  
+
+  constructor() { }
+
+  ngOnInit() {
+   
+  }
+
+ 
+}
